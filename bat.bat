0e1c6309d031da4e284fd26b9dfc6daf8f77b2bf
Knucklesfan <bigfootreserchersoftexas@gmail.com>
2017-04-06 13:56:11 -0500

    Simple test script for testing new features.

---
+++ bat.bat
@@ -0,0 +1,13 @@
+echo Welcome to my test script!
+pause
+color 4c
+timeout 0 >nul
+color f7
+timeout 0 >nul
+color 4c
+timeout 0 >nul
+color f7
+timeout 0 >nul
+color
+echo I hope that looked OK.
+pause
